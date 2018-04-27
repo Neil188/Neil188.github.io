3124258f51a0aa6d9f9c0a673460ab824cca90f3
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-04-28 00:58:39 +0100

    Feat: split lazy load into css+avatar

---
+++ scripts/lazy-load-css.js
@@ -0,0 +1,12 @@
+( () => {
+    // stylesheets
+    const style =  document.createElement('link');
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    style.href = 'style2.css';
+    const font =  style.cloneNode(false);
+    font.href = 'https://cdn.rawgit.com/tonsky/FiraCode/1.205/distr/fira_code.css';
+    const firstStyle = document.querySelector('[rel="stylesheet"]');
+    firstStyle.parentNode.appendChild(style);
+    firstStyle.parentNode.appendChild(font);
+})();
